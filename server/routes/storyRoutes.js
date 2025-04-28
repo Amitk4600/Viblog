dd6666905907f65523920d384a02595061fb2c8e
Amitk4600 <amitk4600@gmail.com>
2025-04-28 15:40:24 +0530

    create route

--- server/routes/storyRoutes.js
+++ server/routes/storyRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createStory } from '../controllers/storyControllers.js';
+import { isAuthenticated } from '../middleware/auth.js';
 const router = express.Router();
 
-router.post("/story/create", createStory);
+router.post("/story/create",isAuthenticated, createStory);
 export default router;
